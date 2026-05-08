3cd8bd13048e114396bfbcdadeaf9e067c311827
Tien Minh Cao <caotienminh2006@gmail.com>
2026-05-08 23:11:04 +0700

    update

--- src/main/java/com/qr_project/qrcode/service/encoding/DataEncoding.java
+++ src/main/java/com/qr_project/qrcode/service/encoding/DataEncoding.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class DataEncoding {
+    private static final String TERMINATOR = "0000";
+
     private static final String MODE_NUMERIC = "0001";
     private static final String MODE_ALPHANUMERIC = "0010";
     private static final String MODE_BYTE = "0100";
@@ -11,7 +13,7 @@ public class DataEncoding {
     private static final String NUMERIC_CHARSET = "0123456789";
     private static final String ALPHANUMERIC_CHARSET = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ $%*+-./:";
     private static final String BYTE_CHARSET = "ISO-8859-1";
-    
+
     public boolean isNumeric(String data) {
         for (char c : data.toCharArray()) {
             if (NUMERIC_CHARSET.indexOf(c) == -1) {
@@ -47,5 +49,18 @@ public class DataEncoding {
         }
         sb.append(binaryString);
         return sb.toString();
+    }
+
+    // Mã hóa dữ liệu
+    public String encodeData(String data) {
+        if (isNumeric(data)) {
+            return MODE_NUMERIC + toBinary(data.length(), 10) + encodeNumeric(data) + TERMINATOR;
+        } else if (isAlphanumeric(data)) {
+            return MODE_ALPHANUMERIC + toBinary(data.length(), 9) + encodeAlphanumeric(data) + TERMINATOR;
+        } else if (isByte(data)) {
+            return MODE_BYTE + toBinary(data.length(), 8) + encodeByte(data) + TERMINATOR;
+        } else {
+            throw new IllegalArgumentException("Unsupported data format");
+        }
     }
 }
