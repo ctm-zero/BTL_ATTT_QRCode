8e714f1d9fa0cd685b5455a458d369eeea2c913d
Tien Minh Cao <caotienminh2006@gmail.com>
2026-05-09 22:17:15 +0700

    refactor: extract constants and tables to QRConstants and QRTable utils

---
+++ src/main/java/com/qr_project/qrcode/utils/QRConstants.java
@@ -0,0 +1,8 @@
+package com.qr_project.qrcode.utils;
+
+public class QRConstants {
+    public static final String[] ERROR_CORRECTION_LEVELS = {"L", "M", "Q", "H"};
+    public static final String PAD_CODEWORD_1 = "11101100";
+    public static final String PAD_CODEWORD_2 = "00010001";
+    public static final int PRIMITIVE_POLYNOMIAL = 285;
+}
